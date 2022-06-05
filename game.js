6ac884d4b71d412c99a40b68dbed80c4ca5017ca
nicksandoval <sand8val@gmail.com>
2022-06-05 13:20:13 -0500

    initial working, game logic ok

---
+++ game.js
@@ -0,0 +1,117 @@
+
+
+class Game {
+    constructor(playerRed, playerBlack, whoStarts) {
+        this.playerRed = playerRed;
+        this.playerBlack = playerBlack;
+        this.currentTurn = whoStarts;
+        this.gameBoard = {
+            f: ["", "", "", "", "", "", ""],
+            e: ["", "", "", "", "", "", ""],
+            d: ["", "", "", "", "", "", ""],
+            c: ["", "", "", "", "", "", ""],
+            b: ["", "", "", "", "", "", ""],
+            a: ["", "", "", "", "", "", ""]
+        };
+    }
+
+    showGameBoard() {
+        return this.gameBoard;
+    }
+
+    addToBoard(num) {
+        const reversedKeys = Object.keys(this.gameBoard).reverse();
+        for (const key of reversedKeys) {
+            if (!this.gameBoard[key][num]) {
+                const indexOfKey = reversedKeys.indexOf(key);
+                this.gameBoard[key][num] = this.currentTurn;
+                this.checkToWin(indexOfKey, num, this.currentTurn);
+                break;
+            }
+        }
+        this.currentTurn === this.playerRed
+            ? this.currentTurn = this.playerBlack
+            : this.currentTurn = this.playerRed;
+    }
+
+    checkToWin(y, x, player) {
+        const length = Object.keys(this.gameBoard).length - 1;
+        this.checkBottom(x + y, player);
+        this.checkTop(length - y + x, player);
+        this.checkHori(y, player);
+        this.checkVert(y, x, player)
+    }
+
+    checkBottom(num, player) {
+        const reversedKeys = Object.keys(this.gameBoard).reverse();
+        let count = 0;
+        for (const key of reversedKeys) {
+            if (num >= 0) {
+                if (this.gameBoard[key][num] === player) {
+                    count++
+                    if (count === 4) {
+                        console.log(player + " " + count)
+                    }
+                } else {
+                    count = 0;
+                }
+            }
+            num--;
+        }  
+    }
+
+    checkTop(num, player) {
+        const keys = Object.keys(this.gameBoard);
+        let count = 0;
+        for (const key of keys) {
+            if (num >= 0) {
+                if (this.gameBoard[key][num] === player) {
+                    count++
+                    if (count === 4) {
+                        console.log(player + " " + count)
+                    }
+                } else {
+                    count = 0;
+                }
+            }
+            num--;
+        }  
+    }
+
+    checkHori(num, player) {
+        const reversedKeys = Object.keys(this.gameBoard).reverse()
+        const key = reversedKeys[num];
+        let count = 0;
+        for (let i = 0; i < this.gameBoard[key].length; i++) {
+            if (this.gameBoard[key][i] === player) {
+                count++
+                if (count === 4) {  
+                    console.log(player + " " + count)
+                }
+            } else {
+                count = 0;
+            }
+        }
+    }
+
+    checkVert(num, col, player) {
+        const reversedKeys = Object.keys(this.gameBoard).reverse();
+        const indexOf = reversedKeys.indexOf(reversedKeys[num]);
+        let count = 0;
+        for (let i = indexOf; i > -1; i--) {
+            console.log(reversedKeys[i])
+            if (this.gameBoard[reversedKeys[i]][col] === player) {
+                count++
+                if (count === 4) {  
+                    console.log(player + " " + count)
+                }
+            } else {
+                count = 0;
+            }
+        }
+    }
+}
+
+
+
+module.exports = Game
---
+++ test/test.js
@@ -0,0 +1,48 @@
+const Game = require('../game.js');
+
+
+
+const newGame = new Game("joe", "bob", "joe");
+
+// newGame.addToBoard(0)
+// newGame.addToBoard(0)
+// newGame.addToBoard(0)
+// newGame.addToBoard(0)
+// newGame.addToBoard(1)
+// newGame.addToBoard(1)
+// newGame.addToBoard(4)
+// newGame.addToBoard(1)
+// newGame.addToBoard(1)
+// newGame.addToBoard(6)
+// newGame.addToBoard(2)
+// newGame.addToBoard(2)
+// newGame.addToBoard(2)
+// newGame.addToBoard(3)
+// newGame.addToBoard(3)
+
+// newGame.addToBoard(4)
+// newGame.addToBoard(5)
+// newGame.addToBoard(5)
+// newGame.addToBoard(3)
+// newGame.addToBoard(4)
+// newGame.addToBoard(6)
+// newGame.addToBoard(3)
+
+// newGame.addToBoard(4)
+// newGame.addToBoard(2)
+// newGame.addToBoard(0)
+// newGame.addToBoard(3)
+// newGame.addToBoard(3)
+// newGame.addToBoard(3)
+// newGame.addToBoard(3)
+
+newGame.addToBoard(1)
+newGame.addToBoard(6)
+newGame.addToBoard(1)
+newGame.addToBoard(6)
+newGame.addToBoard(1)
+newGame.addToBoard(6)
+newGame.addToBoard(1)
+newGame.addToBoard(6)
+
+console.table(newGame.showGameBoard());
